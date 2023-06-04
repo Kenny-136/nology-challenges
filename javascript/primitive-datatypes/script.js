18eaea50cb03538647945b74f5fd1fc19f28d88c
Kenny <kenny@XiZi-M2AIR.local>
2023-06-05 09:42:31 +1000

    completed primitive-datatypes challenge

---
+++ javascript/primitive-datatypes/script.js
@@ -0,0 +1,13 @@
+console.log(typeof 15); // Prediction: {your answer here}
+console.log(typeof 5.5); // Prediction: ...
+console.log(typeof NaN); // Prediction:
+console.log(typeof 'hello'); // Prediction:
+console.log(typeof true); // Prediction:
+console.log(typeof (1 != 2)); // Prediction:
+console.log(typeof ('hamburger' + 's')); // Prediction:
+console.log(typeof ('hamburgers' - 's')); // Prediction:
+console.log(typeof ('1' + '3')); // Prediction:
+console.log(typeof ('1' - '3')); // Prediction:
+console.log(typeof ('johnny' + 5)); // Prediction:
+console.log(typeof ('johnny' - 5)); // Prediction:
+console.log(typeof (99 * 'luftbaloons')); // Prediction:
