1086decbe9183de9c56820492b07b228d8a5fec1
Kenny <kenny@XiZi-M2AIR.local>
2023-06-05 09:44:46 +1000

    completed primitive-datatypes challenge

--- javascript/primitive-datatypes/script.js
+++ javascript/primitive-datatypes/script.js
@@ -1,13 +1,13 @@
-console.log(typeof 15); // Prediction: {your answer here}
-console.log(typeof 5.5); // Prediction: ...
-console.log(typeof NaN); // Prediction:
-console.log(typeof 'hello'); // Prediction:
-console.log(typeof true); // Prediction:
-console.log(typeof (1 != 2)); // Prediction:
-console.log(typeof ('hamburger' + 's')); // Prediction:
-console.log(typeof ('hamburgers' - 's')); // Prediction:
-console.log(typeof ('1' + '3')); // Prediction:
-console.log(typeof ('1' - '3')); // Prediction:
-console.log(typeof ('johnny' + 5)); // Prediction:
-console.log(typeof ('johnny' - 5)); // Prediction:
-console.log(typeof (99 * 'luftbaloons')); // Prediction:
+console.log(typeof 15); // Prediction: number
+console.log(typeof 5.5); // Prediction: number
+console.log(typeof NaN); // Prediction: NaN
+console.log(typeof 'hello'); // Prediction: String
+console.log(typeof true); // Prediction: Boolean
+console.log(typeof (1 != 2)); // Prediction: Truse
+console.log(typeof ('hamburger' + 's')); // Prediction: hamburgers 
+console.log(typeof ('hamburgers' - 's')); // Prediction: NaN
+console.log(typeof ('1' + '3')); // Prediction: 13
+console.log(typeof ('1' - '3')); // Prediction: NaN
+console.log(typeof ('johnny' + 5)); // Prediction: johnny5
+console.log(typeof ('johnny' - 5)); // Prediction: NaN
+console.log(typeof (99 * 'luftbaloons')); // Prediction: NaN
