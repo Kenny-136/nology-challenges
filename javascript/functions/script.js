be278982247f8adb6650754ba3b1574cfe3c0ea2
Kenny <kenny136@outlook.com.au>
2023-06-07 11:25:05 +1000

    refactor to 1 line code

--- javascript/functions/script.js
+++ javascript/functions/script.js
@@ -10,35 +10,25 @@ console.log(moonOrbits(365))
 // Working with Circles
 // -------------------
 
-const circleArea = (radius) => {
-  return Math.PI * (radius ** 2)
-}
+const circleArea = (radius) =>  Math.PI * (radius ** 2)
+const circlePerimeter = (radius) => 2 * Math.PI * radius
 
-const circlePerimeter = (radius) => {
-  return 2 * Math.PI * radius
-}
 console.log(circleArea(10))
 console.log(circlePerimeter(10))
 
 // Years to Days & Seconds
 // ------------------------
 
-const ageInDays = (age) => {
-  return age * 365.2425
-}
+const ageInDays = (age) => age * 365.2425
+const ageInSeconds = (age) =>  age * 31644000
 
-const ageInSeconds = (age) => {
-  return age * 31644000
-}
 console.log(ageInDays(32))
 console.log(ageInSeconds(32))
 
 // Return the Remainder from two numbers
 // --------------
 
-const remainder = (num1, num2) => {
-  return num1 % num2
-}
+const remainder = (num1, num2) => num1 % num2
 console.log(remainder(1, 3))
 console.log(remainder(3, 4))
 console.log(remainder(-9, 45))
@@ -46,16 +36,12 @@ console.log(remainder(5, 5))
 
 // Basketball Points
 // -----------------
-const totalScore = (twoPointer, threePointers) => {
-  return (twoPointer * 2) + (threePointers * 3)
-}
+const totalScore = (twoPointer, threePointers) => (twoPointer * 2) + (threePointers * 3)
 console.log(totalScore(2, 4))
 
 // Less than 100?
 // --------------
-const lessThan100 = (num1, num2) => {
-  return (num1 + num2 < 100) ? true : false
-}
+const lessThan100 = (num1, num2) => (num1 + num2 < 100) ? true : false
 console.log(lessThan100(22,15))
 console.log(lessThan100(83,34))
 console.log(lessThan100(3,77))
